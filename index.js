bb4eac05b18a2b99490c03064fb7efddebd6e814
Tsvetan Iliev <tsvetanilievdev@gmail.com>
2022-01-25 19:01:40 +0300

    run server

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const app = express();
 
-app
+app.get('/', (req, res) => {
+    res.write('Hello');
+    res.end();
+})
+
+app.listen(5000, () => console.log('Cubicle is working on port 5000...'));
