8a9c94a6be0dde8a91d0c6bf03fbcfd522c84fae
Tsvetan Iliev <tsvetanilievdev@gmail.com>
2022-01-25 22:05:15 +0300

    add controllers

---
+++ controllers/cubeController.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const createCube = (req, res) => {
+    res.render('create');
+}
+
+router.get('/create', createCube);
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
+
 const unitHandlerbars = require('./config/handlebars');
+const routes = require('./routes');
+
 const app = express();
 
-// require('./config/handlebars')(app); вместо --> const unitHandlerbars = require('./config/handlebars'); unitHandlerbars(app);
 unitHandlerbars(app);
-app.use('/static',express.static('static'))
 
-app.get('/', (req, res) => {
-    res.render('index')
-})
+app.use('/static', express.static('static'));
+app.use(routes);
 
 app.listen(5000, () => console.log('Cubicle is working on port 5000...'));
