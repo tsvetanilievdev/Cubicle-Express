ea3403224c3770ce80e49c43959f3dce261544b0
Tsvetan Iliev <tsvetanilievdev@gmail.com>
2022-01-27 15:01:16 +0300

    add details page

--- controllers/cubeController.js
+++ controllers/cubeController.js
@@ -6,6 +6,18 @@ const renderCreateCube = async (req, res) => {
   res.render('create');
 };
 
+const cubeDetails = async (req, res) => {
+  const cubes = await cubeService.getAll();
+  let cube = cubes.find(x => x.id == req.params.id);
+  console.log(cube)
+  if(!cube){
+    return res.redirect('/error');
+  }
+  res.render('details', {
+    ...cube
+  })
+
+}
 
 const createCube = async (req, res) => {
   let { name, description, imageUrl, difficulty } = req.body;
@@ -14,6 +26,10 @@ const createCube = async (req, res) => {
   res.redirect('/cube/create');
 };
 
-router.route('/create').get(renderCreateCube).post(createCube);
+router.route('/create')
+.get(renderCreateCube)
+.post(createCube);
+router.route('/details/:id')
+.get(cubeDetails)
 
 module.exports = router;
